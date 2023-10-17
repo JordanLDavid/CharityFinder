0236f87905511a2e3f7b7aec25647f83098bfd91
Jordan Lenard David <jordan_david02@hotmail.com>
2023-10-16 22:47:47 -0400

    [u] Fix layout of charities shown

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -15,16 +15,22 @@ export const SearchResult = () => {
           setSearchResults(response.data);
         })},[]);
 
-    return (<>
+        return (<>
             <div>
-                <ul className="columns-3">
+                <ul className="m-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-1">
                 {results?.nonprofits.map((data)=> 
-                <div>
+                <div className="border border-solid border-gray-300 p-2 grid grid-rows-2">
+                  <div className="min-content items-center flex justify-center p-2">
                     { (data.logoUrl !== undefined && data.logoUrl !== '' ?
-                        <img src={data.logoUrl}></img> : 
-                        <img src="../GenericIcon.png" width="48" height="48"/> )}
-                    <Link to={"/charity/"+data.ein}>{data.name}</Link>
+                      <img src={data.logoUrl}></img> : 
+                      <img src="GenericIcon.png" width="48" height="48"/> )}
+                      </div>
+                      <div className="gap-2 row-span-1 col-span-1 flex items-center justify-start">
+                      <Link to={"/charity/"+data.ein}>{data.name}</Link>
+                    </div>
+                    <div className="row-span-1 col-span-2 flex items-start justify-center">
                     <p>{data.location}</p>
+                    </div>
                 </div>
                 )}
           </ul>
